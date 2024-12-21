3f7fa09de7ad23d409788b58872099bd62bd52df
krish15soni <krish1509soni@gmail.com>
2024-12-21 23:26:22 +0530

    Chatzy prodution

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,21 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
 			keyframes: {
 				typing: {
-					'from': { width: '0%' },
-					'to': { width: '100%' }
+					from: { width: "0%" },
+					to: { width: "100%" },
 				},
 				blink: {
-					'50%': { borderColor: 'transparent' }
-				}
+					"50%": { borderColor: "transparent" },
+				},
 			},
 			animation: {
-				typing: 'typing 3s steps(30, end) forwards, blink .75s step-end infinite'
-			}
+				typing: "typing 3s steps(30, end) forwards, blink .75s step-end infinite",
+			},
 		},
 	},
-	plugins: [require("daisyui")],
+	plugins: [daisyui],
 };
