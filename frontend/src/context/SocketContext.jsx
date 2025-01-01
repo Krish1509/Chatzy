81260514e47413f2d645ede239f3f2236b9fa8df
krish15soni <krish1509soni@gmail.com>
2025-01-01 12:38:14 +0530

    Chatzy_Test

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 	useEffect(() => {
 		if (authUser) {
 			//https://chatzy-b1j0.onrender.com
-			const socket = io("http://localhost:5000/", {
+			const socket = io("/https://chatzy-b1j0.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
